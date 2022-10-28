e20a2d6f407b71f3d47e2ba8bdb0ffbb045811c8
Hamza192022 <hamzapersonal12@gmail.com>
2022-10-28 15:32:38 +0500

    complete project in react

---
+++ src/components/resturent_components/Resturent.js
@@ -0,0 +1,45 @@
+import React , {useState} from 'react'
+import "./style.css"
+import Menu from "./ManuApi";
+import MenuCard from "./ManuCard";
+import Navbar from "./Navbar";
+
+
+const uniqueList = [
+    ...new Set(
+      Menu.map((curElem) => {
+        return curElem.category;
+      })
+    ),
+    "All",
+  ];
+  
+  console.log(uniqueList);
+
+const Resturent = () => {
+    const [menuData, setMenuData] = useState(Menu);
+    const [menuList, setMenuList] = useState(uniqueList);
+
+    
+    const filterItem = (category) => {
+        if (category === "All") {
+          setMenuData(Menu);
+          return;
+        }
+    
+        const updatedList = Menu.filter((curElem) => {
+          return curElem.category === category;
+        });
+    
+        setMenuData(updatedList);
+      };
+
+  return (
+    <>
+     <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
+    </>
+  )
+}
+
+export default Resturent
